a6cf52c72efbc7930f1879c82ebd7b78012843a4
LaxOS147 <LaxOS147@proton.me>
2026-09-17 11:06:35 +0100

    Dev Rev #1

---
+++ LaxOS.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 10:25:36 2026
+
+@author: EMcEnery
+"""
+
+def LaxOS():
+    
+    import sqlalchemy
+    import OSError
+    import locals
+    import networkx
+    
+    Reliability = PP
+    Redundency  = hm/aw
+    rewardSystem = Add/c
+    CopyDirectory = Btz
+    
+    sqlalchemy (Wb, cb)
+    OSError (Bp, bp)
+    locals (Wb, Bp)
+    networkx (cb, pb)
+    
+    Wb(Add(wd),hm(e),PP(ap), Btz(d))
+    Bp(Add(fu),hm(e),PP(hp), Btz(U))
+    cb(C(wu),aw(e),PP(ap), Btz(U))
+    bp(C(fd),aw(e),PP(hp), Btz(d))
+    
+    return sum(Wb, BP, cb, bp)
+    
+
+LaxOS()
+    
