bdea8fa00ec7eda7c9ae93bab3ccca7fc19c4cab
LaxOS147 <LaxOS147@proton.me>
2026-09-17 15:50:54 +0100

    Add files via upload

---
+++ closeCall.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+def CloseCall():
+    
+    import build
+    import ted
+    import gopher
+    import OSError
+    import fireWallGui
+    
+    Calc=Ct, 
+    dimE=VErn,
+    lines=SOB, 
+    pin=phy(n)
+    Up=Ind, 
+    Weight=ERng, 
+    Drives=ORng
+    zer0=0
+    
+
+    build(Ct(y), VErn(y), SOB(y))
+    ted(Ct(n), VErn(n), SOB(n), phy(n))
+    gopher(Ind(y), ERng(n), ORng(y))
+    OSerror(Ind(n), ERng(y), phy(y), ORng(n))
+    
+    print('top')
+    
+    return build, ted, gopher, OSerror
+
+
+CloseCall()
