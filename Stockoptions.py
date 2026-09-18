bf47890e8b8b63bf668a8acfa38291b75d8fed3c
LaxOS147 <LaxOS147@proton.me>
2026-09-18 18:53:28 +0100

    Add files via upload

---
+++ Stockoptions.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+
+def st0ck0ption():
+    
+    import costFilter as redPurse
+    import geigerC as coinCrypt
+    import claW as cliM8
+    import crabHouse as gPath
+
+    import RunCycleSwim
+    import pillaR
+
+    PTS = oPo, 
+    HVAC = temp,
+    Hypno = sL,
+    VI = GPS,
+    Ronnie = pos,
+    Fix = res,
+    DDay= fPos,
+    Cap = st,
+    GC = Bond,
+    JRa= raJ,
+    MM = mini
+
+    PowerAnalisis=(SL+oPO+GPS+[raJ, fPOS, pos]+Temp+St+mini)
+
+    redPurse(oPo(o), temp(h), sL(f), GPS(I), st(y), raJ(s))
+    coinCrypt(res(t), pos(s), temp(h), GPS(O), st(n), raJ(j))
+    cliM8(oPo(p), temp(c), sL(m),GPS(O), fPos(s), mini(y))
+    gPath(res(p), pos(j), temp(c), GPS(I), fPos(j), mini(n))
+
+
+def pillaR():
+    import cryptCoin
+    import doghouSe
+    import locATE
+    from Cartesian import Rand, ReadeR, polDA
+    import Sillycab
+    import Random
+    import Rats
+
+
+def RunCycleSwim():
+    x = CartesianLocation + Coolbreeze + blackMirror
