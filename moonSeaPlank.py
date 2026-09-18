27c19fbd70cfc3ed0459bd2eef5a9f4e5d2c0d68
LaxOS147 <LaxOS147@proton.me>
2026-09-18 10:32:24 +0100

    Add files via upload

---
+++ moonSeaPlank.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+
+def moonSeaPlank():
+    
+    import rotoFree as apple
+    import hash as puff
+    import nordIP as butter
+    import conCH as sea
+
+    Kg=PL
+    cW=FStr
+    Fsh=Sn
+    Bmc=Ω
+    Hll=T
+    Jc=GG
+    Dubz=nP
+
+
+    apple(PL(d), FStr, Ω(o), Tj(d))
+    puff(PL(u), FStr, Ω(z), nP(d))
+    butter(GG(n), Sn(u), Ω(o), Tc(u))
+    sea(GG(y), Sn(d), Ω(z), nP(u))
+    
+    BBL, bbF = sum(OF,ThC, Nord, Conch)
+    
+    return BBL, bbF
