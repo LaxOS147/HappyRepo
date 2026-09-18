0cf8fe88498ec668ecc7b366689f971e33b482a8
LaxOS147 <LaxOS147@proton.me>
2026-09-18 10:18:39 +0100

    Add files via upload

---
+++ moonSeaPlank.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+
+def moonSeaPlank():
+    
+    import rotoFree
+    import hash
+    import nordIP
+    import conCH
+
+    Kg=PL
+    cW=FStr
+    Bmc=Ω
+    Jc=GG
+    Fsh=Sn
+    Hll=T
+    Dubz=nP
+
+
+    rotoFree(PL(d), FStr, Ω(o), Tj(d))
+    hash(PL(u), FStr, Ω(z), nP(d))
+    nordIP(GG(n), Sn(u), Ω(o), Tc(u))
+    conCH(GG(y), Sn(d), Ω(z), nP(u))
+    
+    BBL, bbF = sum(OF,ThC, Nord, Conch)
+    
+    return BBL, bbF
