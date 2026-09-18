2b8ae12f7cb49f3947c6982ad3d66ba443adc328
LaxOS147 <LaxOS147@proton.me>
2026-09-18 12:07:04 +0100

    Add files via upload

---
+++ closeCall.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+def CloseCall():
+    
+  import build as butter
+  import ted as apple
+  import gopher as glass
+  import OSError
+
+  import fireWallGui
+  import pillaR
+  import tensorInt
+  
+  
+  Calc=Lt, 
+  dimE=Vern 
+  lines=SOB 
+  pin=rtP(n) 
+  Up=Ind 
+  Drives=ORng 
+  Weight=ERng, 
+
+  zer0=0
+  
+  
+  
+  butter(Lt(n), VErn(u), SOB(y), rtP(pst))
+  apple(Lt(y), VErn(d), SOB(n), rtP(pr))
+  
+  glass(Ind(y), ERng(n), ORng(y), rtP(pr))
+  OSerror(Ind(n), ERng(y), ORng(n), rtP(pst))
+
+  HtF = sum(build, ted, gopher, OSerror)
+  print('top')
+  
+  return HtF
+
+
+
+CloseCall()
+
+def pillaR():
+    
+    import GC
+    import lightPlan
+    import gopher
+    import limpEye
+    
+    import lettER
+    import nudGE
+    import guLY
+    import Frozen as Occulus
+    import Seena
+    import Grin
+    import Zight
+    
+
+def tensorInt():
+    
+    x = C + L + V
